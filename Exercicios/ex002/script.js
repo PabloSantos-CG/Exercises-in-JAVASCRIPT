cc621ba21370ac7a4e89c1dc0cefd4bee5148c16
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-05-30 11:44:39 -0300

    update

---
+++ Exercicios/ex002/script.js
@@ -0,0 +1,14 @@
+// let carro = 'ferrari' ; 
+// let bolo = 19.99 ;
+
+// console.log ('Olá! Sua', carro, 'está na garagem e seu bolo custou R$ ', bolo);
+
+var idade = 60;
+
+if (idade < 18){
+    console.log('Você é de MENOR');
+} else if (idade >= 18 && idade <= 59){
+    console.log('Você é de MAIOR');
+} else if (idade >= 60){
+    console.log('Você é IDOSO');
+}
