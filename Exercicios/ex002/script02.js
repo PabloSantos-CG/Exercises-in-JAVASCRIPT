3c63bf147de39246a406f205403705084b32a362
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-05-31 21:04:28 -0300

    upddate

---
+++ Exercicios/ex002/script02.js
@@ -0,0 +1,54 @@
+/*
+
+ let preco = 46;
+ 
+// até 45 está barato
+
+if (preco <= 45){
+    console.log('O preço da carne está barato. APROVEITE :)');
+} else{
+    console.log('O preço está caro!');
+}
+
+ */
+
+/*
+
+let profession = 'piloto';
+
+console.log('Profissão: ', profession);
+
+switch(profession){
+  case 'fiscal':
+    console.log('Sua camisa é Verde');
+    break;
+
+  case 'policial':
+    console.log('Sua camisa é Azul');
+    break;
+
+  case 'bombeiro':
+    console.log('Sua camisa é Vermelha');
+    break;
+
+  default:
+    console.log('Profissão não cadastrada')
+}
+
+*/
+
+/*
+function media(av1, av2, av3){
+  let md = (av1 + av2 + av3)/3;
+  console.log ('Olá, o resultado da sua média final é: ', md);
+}
+
+media(4, 9, 4);
+
+*/
+
+function soma(n1, n2){
+  tot = n1 + n2;
+  console.log('Resultado da soma é: ', tot);
+}
+soma(10, 5);
