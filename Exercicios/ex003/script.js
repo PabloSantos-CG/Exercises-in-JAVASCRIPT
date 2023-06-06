4683e0da742c4c3cc4a5bc3a06de85474a2d7c51
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-06-06 17:23:06 -0300

    update

--- Exercicios/ex003/script.js
+++ Exercicios/ex003/script.js
@@ -1,13 +1,32 @@
-function SomaRaizQ (n1, n2){
-    const Block = (x) => x * x;
+// function SomaRaizQ (n1, n2){
+//     const Block = (x) => x * x;
     
-      let nBlock1 = Block(n1);
-      let nBlock2 = Block(n2);
-      let tot = nBlock1 + nBlock2;
+//       let nBlock1 = Block(n1);
+//       let nBlock2 = Block(n2);
+//       let tot = nBlock1 + nBlock2;
     
-      console.log('A soma da raiz quadrada de ', n1, '+', n2, 'é', tot);
-    }
+//       console.log('A soma da raiz quadrada de ', n1, '+', n2, 'é', tot);
+//     }
     
-    SomaRaizQ(2,3);
+//     SomaRaizQ(2,3);
     
     // let exemple ['position1', 2, 'position3', [5,6,7] ];
+
+    let consessionaria = {
+      chefiaGeral: 'Sun Tzu',
+      valuetion: 12000000.6,
+      localizacao: 'Belo Horizonte',
+      unidade: [ 
+        {id: 0, nomeUnidade: 'NovoTempo', gestor: 'Paulo', localizacao: 'Salvador', estoqueGeral: 90},
+    
+        {id: 1, nomeUnidade: 'VelhoTempo', gestor: 'Ana', localizacao: 'Lauro de Freitas', estoqueGeral: 30},
+    
+        {id: 2, nomeUnidade: 'HondaOrigi', gestor: 'Cleiton', localizacao: 'Camaçari', estoqueGeral: 235}
+      ]
+    }
+    
+    console.log('A chefia Geral desta empresa se chama: ' + consessionaria.chefiaGeral);
+    
+    console.log('nosso valuetion representa atualmente R$' + consessionaria.valuetion);
+    
+    console.log('Nossa unidade com maior estoque é',  consessionaria.unidade[2].nomeUnidade, 'e seu estoque equivale à: ', consessionaria.unidade[2].estoqueGeral, 'UNIDADES');
