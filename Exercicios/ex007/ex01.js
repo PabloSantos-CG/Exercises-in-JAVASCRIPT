737ef6fd9ac87191565dfa7e615f4c422141d243
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-06-13 18:40:37 -0300

    update

--- Exercicios/ex007/ex01.js
+++ Exercicios/ex007/ex01.js
@@ -1,44 +1,34 @@
-/*
-  1º Menu c/5 opções diferentes
-  2º Iniciar mostrando as 5 opções disponíveis
-  3º 4 primeiras sem funcionalidades
-  4º A última deve ser a opção 'ENCERRAR'
-  5º Ao clicar em alguma das 4 primeiras uma msg deve ser exibida
-  depois o programa deve voltar a exibir novamente o menu com as 5 opções
-  6º Deve continuar até que o usuário escolha 'ENCERRAR'
-  7º Se, escolhe-la uma msg deve ser exibida dizendo que o      sistema está sendo encerrado e entao, encerrar
-*/
-loop = "Continuar";
-do {
-  let menu = prompt(
-    "---- MENU ----" +
-      "\nOption1" +
-      "\nOption2" +
-      "\nOption3" +
-      "\nOption4" +
-      "\nEncerrar"
-  );
+let option = '';
 
-  switch (menu) {
-    case "Option1":
-      alert("chosen Option1");
-      break;
+do{
+  option = parseFloat(prompt(
+  'Olá! Seja Bem-Vindo(a)' +
+  '\nEscolha o número da opção desejada:' +
+  '\n1. Opção Um' +
+  '\n2. Opção Dois' +
+  '\n3. Opção Treis' +
+  '\n4. Opção Quatro' +
+  '\n5. Sair'
+));
 
-    case "Option2":
-      alert("chosen Option2");
-      break;
-
-    case "Option3":
-      alert("chosen Option3");
-      break;
-
-    case "Option4":
-      alert("chosen Option4");
-      break;
-
-    case "Encerrar":
-      alert("Encerrando!");
-      loop = "Encerrar";
-      break;
+  switch (option){
+    case 1:
+      alert('Você escolheu: ' + option + 'º Opção');
+    break
+    case 2:
+      alert('Você escolheu: ' + option + 'º Opção');
+    break
+    case 3:
+      alert('Você escolheu: ' + option + 'º Opção');
+    break
+    case 4:
+      alert('Você escolheu: ' + option + 'º Opção');
+    break
+    case 5:
+      alert('Você escolheu: ' + option + 'º Opção');
+      alert('Encerrando...');
+    break
+    default:
+      ('Opção inválida!');
   }
-} while (loop === "Continuar");
+} while(option !== 5)
---
+++ Exercicios/ex007/ex02.js
@@ -0,0 +1,30 @@
+let option = '';
+let balance = parseFloat(prompt(
+  'Seja Bem-Vindo!' +
+  '\nInsira seu saldo:'
+));
+
+do{
+  option = parseFloat(prompt(
+    'Saldo Disponível: R$ ' + balance +
+    '\nOpções:' +
+    '\n1. Adicionar Dinheiro' +
+    '\n2. Remover Dinheiro' +
+    '\n3. Sair'
+  ));
+
+  switch (option){
+    case 1:
+      balance += parseFloat(prompt('Adicionar: R$ '));
+    break
+    case 2:
+      balance -= parseFloat(prompt('Remover: R$ '));
+    break
+    case 3:
+      alert('Encerrando...');
+    break
+    default:
+      alert('Opção inválida!');
+  }
+
+} while(option !== 3)
