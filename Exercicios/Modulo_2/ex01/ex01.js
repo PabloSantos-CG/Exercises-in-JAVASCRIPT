b4b8a564d4b13e0235366e0646cb9aba57203a6d
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-06-29 21:51:54 -0300

    update

--- Exercicios/Modulo_2/ex01/ex01.js
+++ Exercicios/Modulo_2/ex01/ex01.js
@@ -1,7 +1,4 @@
-const vagas = [0, 'um', 2]
 function eventos () {
   document.getElementsByTagName('button')
   alert('PARABÉNS! \nVocê resgatou, confira seu e-mail')
-  const rua = vagas[0]
-  vagas.push()
 }
