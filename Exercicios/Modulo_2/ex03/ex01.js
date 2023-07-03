654e969d2a83ff3a23eec762bfd7a63ae254e02b
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-07-03 10:30:04 -0300

    update

--- Exercicios/Modulo_2/ex03/ex01.js
+++ Exercicios/Modulo_2/ex03/ex01.js
@@ -1,7 +1,32 @@
 function addPlayer () {
+  const ul = document.getElementById('list')
+  
   const position = document.getElementById('position').value
   const name = document.getElementById('name').value
   const number = document.getElementById('number').value
 
-  console.log( {position, name, number})
+  const confirmation = confirm ('Deseja inserir o jogador ' + name + ' no time?')
+
+  if(confirmation) {
+    const li = document.createElement('li')
+    li.id = 'player-' + number
+    li.innerText = 'Jogador: ' + name + '\nPosição: ' + position + '\nNúmero: ' + number + '\n\n'
+    ul.appendChild(li)
+
+    document.getElementById('position').value = ''
+    document.getElementById('name').value = ''
+    document.getElementById('number').value = ''
+  }
+}
+
+function removePlayer () {
+  const numberRemove = document.getElementById('remove').value
+  const idPlayerRemove = document.getElementById('player-' + numberRemove)
+  
+  const confirmation = confirm('Deseja excluir do time o seguinte jogador? ' + '\n' + idPlayerRemove.innerText)
+
+  if (confirmation) {
+    document.getElementById('list').removeChild(idPlayerRemove)
+    document.getElementById('remove').value = ''
+  }
 }
