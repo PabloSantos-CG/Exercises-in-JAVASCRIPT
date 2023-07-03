5a84be35d73030b9fae9c14cd425a1ccde980943
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-07-03 17:10:51 -0300

    update

---
+++ Exercicios/Modulo_2/ex04/ex03.js
@@ -0,0 +1,8 @@
+function cursor () {
+  const bloco = document.getElementById('bloco')
+  bloco.style.left='15%'
+}
+function cursorback () {
+  const bloco = document.getElementById('bloco')
+  bloco.style.left='0%'
+}
