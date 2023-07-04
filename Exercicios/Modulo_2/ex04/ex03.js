21ffe939014feefc4201e1763ba246ce624f4c33
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-07-04 10:48:45 -0300

    update

--- Exercicios/Modulo_2/ex04/ex03.js
+++ Exercicios/Modulo_2/ex04/ex03.js
@@ -1,8 +1,33 @@
-const caixaInterativa = document.getElementById('bloco')
-caixaInterativa.addEventListener('mouseenter', function () {
-  this.innerText = 'mouse entrou na box'
-})
+const form = document.getElementById('orderForm')
 
-caixaInterativa.addEventListener('mouseout', function () {
-  this.innerText = 'mouse saiu na box'
+form.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const name = document.getElementById('name').value
+  const address = document.getElementById('address').value
+  const typeOfBread = document.querySelector('select').value
+  const protein = document.querySelector('input[name="main"]:checked').value
+  const comments = document.getElementById('observations').value
+
+  let salad = ''
+  document.querySelectorAll('input[name="salad"]:checked').forEach( function (item) {
+    salad += ' - ' + item.value + '\n'
+  })
+
+  const confirmation = confirm(
+    '- PEDIDO -' +
+    '\n\nNome: ' + name +
+    '\nEndereço: ' + address +
+    '\nTipo de pão: ' + typeOfBread +
+    '\nProteína: ' + protein +
+    '\nSalada: \n' + salad +
+    '\nObservações: ' + comments
+  )
+
+  if (confirmation) {
+    alert('Pedido Confirmado!')
+    console.log( {name, address, typeOfBread, protein, salad, comments})
+  } else {
+    alert('Preencha novamente')
+  }
 })
