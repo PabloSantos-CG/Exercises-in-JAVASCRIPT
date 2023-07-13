bdb0f3baadeb50765645e79cd08c3ce813496fc6
PabloSantos-CG <130402162+PabloSantos-CG@users.noreply.github.com>
2023-07-13 09:13:50 -0300

    update

--- Exercicios/Modulo_2/ex06/ex03.js
+++ Exercicios/Modulo_2/ex06/ex03.js
@@ -1,4 +1,25 @@
-document.getElementById('buttonSim').addEventListener('click', function () {
-  alert("Deus te pague meu bom senhor")
+const nave = [
+  ['Fenix', 8, true],
+  ['Golias', 10, true],
+  ['Helmet', 5, false],
+  ['Elemental', 3, true],
+  ['Darwin', 15, false],
+]
+
+const naveTripulantes = nave.filter( function (valorAtual) {
+  return valorAtual[1] > 9
+}).map( function (valorAtual) {
+  return valorAtual[0]
 })
 
+const plataforma = nave.findIndex( function (value, index) {
+  if (value[2] == false) {
+    return index
+  }
+})
+
+let exibir = ''
+exibir += 'Naves com mais de 9 tripulantes: \n' + naveTripulantes.join('\n')
+exibir += '\nPrimeira plataforma com engate pendente: ' + (plataforma + 1)
+
+alert(exibir)
