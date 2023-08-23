effa47f48df899dbb3cb801719ad91aba8788eee
PabloSantos-CG <comercial-pablo@outlook.com>
2023-08-23 14:20:17 -0300

    update

--- Exercicios/ExerciciosLivres/OOP/index.js
+++ Exercicios/ExerciciosLivres/OOP/index.js
@@ -2,10 +2,10 @@ const LibrariesSet = require("./LibrariesSet");
 
 const librariesSet = new LibrariesSet()
 
-const bibliotecas = librariesSet.Bibliotecas
+const result = librariesSet.result
 
 librariesSet.addLibrarys('Biblioteca UCSal', '(41) 99916-2141', '9h às 18h', 'Rua Coronel Teixeira', '42700-115', 'Salvador', 'Bahia')
 
 librariesSet.addLibrarys('Biblioteca PUC', '(75) 99916-2141', '9h às 13', 'Rua Pinheiro Martins', '42000-015', 'Camaçari', 'Bahia')
 
-console.log(bibliotecas)
+console.log(result)
