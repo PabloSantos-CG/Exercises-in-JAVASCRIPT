972fb2098a1313088d0a38c1b7aace4c9e7ba931
PabloSantos-CG <comercial-pablo@outlook.com>
2023-09-01 17:15:49 -0300

    update

---
+++ Exercicios/ExerciciosLivres/outros/ex01.js
@@ -0,0 +1,7 @@
+const arr1 = [2,3,4,5]
+const arr2 = [...arr1]
+
+arr2.push('Não altera')
+
+console.log(arr1)
+console.log(arr2)
---
+++ Exercicios/ExerciciosLivres/outros/ex02.js
@@ -0,0 +1,4 @@
+let a = 8;
+let b= 2;
+[a, b] = [b, a]
+console.log(a, b)
