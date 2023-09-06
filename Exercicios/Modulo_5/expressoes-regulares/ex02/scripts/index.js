9b94be02425a40d8dffffb4c92fd206d6703bcc2
PabloSantos-CG <comercial-pablo@outlook.com>
2023-09-06 13:59:40 -0300

    Daily exercise

--- Exercicios/Modulo_5/expressoes-regulares/ex02/scripts/index.js
+++ Exercicios/Modulo_5/expressoes-regulares/ex02/scripts/index.js
@@ -1,3 +1,10 @@
+const inputs = {
+  email: document.querySelector("#userEmail"),
+  password: document.querySelector("#userPassword")
+}
+
+
+
 function validateEmail(email) {
   if(!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)) {
     const error = new Error('Email inválido!')
@@ -6,14 +13,61 @@ function validateEmail(email) {
   }
 }
 
+/* 
+        password.length < 8 || 
+        !password.match(/[a-z]/) || 
+        !password.match(/[A-Z]/) || 
+        !password.match(/[0-9]/) ||
+        !password.match(/[^a-zA-Z0-9\s]/)
+*/
+
 function validatePassword(password) {
   if(
-    !password.length < 8 ||
-    !password.match(/a-z/) ||
-    !password.match(/A-Z/) ||
-    !password.match(/0-9/) ||
+    password.length < 8 ||
+    !password.match(/[a-z]/) ||
+    !password.match(/[A-Z]/) ||
+    !password.match(/[0-9]/) ||
     !password.match(/[^a-zA-Z0-9\s]/)
     ) {
-
-  }
+      const error = new Error('Senha inválida!')
+      error.input = 'password'
+      throw error
+  } 
 }
+
+function resetFormStyles() {
+  Object.entries(inputs).forEach(([key, value]) => {
+      value.classList.remove('success', 'error')
+      document.querySelector(`#${key}-error`).textContent = ''
+  })
+}
+
+// document.querySelector("#userEmail").addEventListener("focusout", () => {
+  
+// })
+
+// function inspection() {
+
+// }
+
+document.querySelector("form").addEventListener("submit", (ev) => {
+  ev.preventDefault()
+  resetFormStyles()
+
+  try {
+    validateEmail(inputs.email.value)
+    inputs.email.classList.add('success')
+    validatePassword(inputs.password.value)
+    inputs.password.classList.add('success')
+
+  } catch (error) {
+    inputs[error.input].classList.add('error')
+    document.querySelector(`#${error.input}-error`).textContent = error.message
+
+    console.log(error)
+  } finally {
+    console.log('uma execução foi realizada')
+  }
+})
+
+//melhorias, sempre que o mouse entrar e também adicionar um finally com submit
