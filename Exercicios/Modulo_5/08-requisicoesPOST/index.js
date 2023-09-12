c031959da9ff1c9c0fc3a7aa48f56abb981a7654
PabloSantos-CG <comercial-pablo@outlook.com>
2023-09-12 17:34:14 -0300

    update

--- Exercicios/Modulo_5/08-requisicoesPOST/index.js
+++ Exercicios/Modulo_5/08-requisicoesPOST/index.js
@@ -1,23 +1,4 @@
-function createArticle(element) {
-  const article = document.createElement("article")
-  article.id = element.id
-
-  const title = document.createElement("h3")
-  title.innerText = element.title
-
-  const paragraph = document.createElement("p")
-  paragraph.textContent = element.content
-
-  const anchor = document.createElement("a")
-  anchor.href = "#"
-  anchor.innerText = " Author: " + element.author
-  anchor.classList.add("anchor")
-
-  paragraph.appendChild(anchor)
-  article.append(title, paragraph)
-
-  document.querySelector("#articles").appendChild(article)
-}
+ 
 
 //Método de renderização
 async function render() {
